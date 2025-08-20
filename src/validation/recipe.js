1adcf5e6717a41449547bd1b5f4f01d8b5871c71
Shade <kulakaa073@gmail.com>
2025-08-20 13:07:17 +0300

    smol update

--- src/validation/recipe.js
+++ src/validation/recipe.js
@@ -1,13 +1,7 @@
 import Joi from 'joi';
-import { isValidObjectId } from 'mongoose';
+import { ingredientSchema } from './ingredient.js';
 
 export const createRecipeSchema = Joi.object({
-  userId: Joi.string().custom((value, helper) => {
-    if (value && !isValidObjectId(value)) {
-      return helper.message('User id should be a valid mongo id');
-    }
-    return true;
-  }),
   name: Joi.string().max(64).required().messages({
     'string.base': 'Name must be a string',
     'string.min': 'Name should have at least {#limit} characters',
@@ -18,23 +12,25 @@ export const createRecipeSchema = Joi.object({
     'string.base': 'Description must be a string',
     'string.max': 'Description should have at most {#limit} characters',
   }),
-  ingredients: Joi.array()
-    .items(
-      Joi.object({
-        name: Joi.string().custom((value, helper) => {
-          if (value && !isValidObjectId(value)) {
-            return helper.message('User id should be a valid mongo id');
-          }
-          return true;
-        }),
-        quantity: Joi.string().messages({
-          'string.base': 'Ingredient quantity must be a string',
-          'any.required': 'Ingredient quantity is required',
-        }),
+  ingredients: Joi.alternatives()
+    .try(
+      Joi.array().items(ingredientSchema).min(2).max(16),
+      Joi.string().custom((value, helper) => {
+        try {
+          const arr = JSON.parse(value);
+          if (!Array.isArray(arr)) throw new Error();
+          const { error } = Joi.array()
+            .items(ingredientSchema)
+            .min(2)
+            .max(16)
+            .validate(arr);
+          if (error) return helper.message(error.message);
+          return arr;
+        } catch {
+          return helper.message('Ingredients must be a valid JSON array');
+        }
       }),
     )
-    .min(2)
-    .max(16)
     .required()
     .messages({
       'array.base': 'Ingredients must be an array',
@@ -45,14 +41,7 @@ export const createRecipeSchema = Joi.object({
     'any.required': 'Instructions are required',
     'string.max': 'Instructions should have at most {#limit} characters',
   }),
-  category: Joi.string()
-    .required()
-    .custom((value, helper) => {
-      if (value && !isValidObjectId(value)) {
-        return helper.message('User id should be a valid mongo id');
-      }
-      return true;
-    }),
+  category: Joi.string().required(),
   cookingTime: Joi.string().min(1).max(360).required().messages({
     'string.base': 'Cooking time must be a string',
     'any.required': 'Cooking time is required',
